a0be423723472c78e99c9ad4335dcf234095ab19
Larry Fry <larryshadow417@gmail.com>
2017-05-16 14:09:40 -0500

    Make the home page look pretty

--- home/home.php
+++ home/home.php
@@ -1,9 +1,10 @@
 <?php include("./view/nav.php"); ?>
+
   <div class="paintballCaroselWrapper">
     <div class="paintballCarosel">
-      <img src="http://placekitten.com/g/688/144" alt="">
-      <img src="http://placekitten.com/g/688/144" alt="">
-      <img src="http://placekitten.com/g/688/144" alt="">
+      <img id="caroselPic" src="images/paintFace.jpg" alt="">
+      <img id="caroselPic" src="images/dive.jpg" alt="">
+      <img id="caroselPic" src="images/victory.jpg" alt="">
     </div>
   </div>
   <!-- Jquery -->
@@ -22,4 +23,18 @@
       });
     });
   </script>
+  <div class="homeDiscription">
+    <h1>We have the Gear you need!</h1>
+    <h3>John Paintson</h3>
+    <h4>Local Customer/National Champ</h4>
+    <div id="customerReview">
+      <img src="images/player.jpg" alt="" />
+      <p>I'm John Paintson and I've been playing paintball since I was 3 years old.</p>
+      <p>Super Doot Paintball has been an amazing supplier of the gear I need to win.</p>
+      <p>Super Doot has earned my trust and they deserve yours aswell.</p>
+
+    </div>
+  </div>
+
+
 <?php include("./view/footer.php"); ?>
--- view/footer.php
+++ view/footer.php
@@ -1,7 +1,7 @@
 <div class="pre-footer"></div><!-- Pretty Little Color Thing.-->
 
   <footer>
-    <h2>I'm the footer.</h2>
+    <h2>Super Doot Paintball &#169;2017</h2>
   </footer>
 </body>
 </html>
